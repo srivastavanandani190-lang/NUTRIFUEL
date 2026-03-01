24f462ef2b24af3b91b8f1eebeb339022bd94386
miaoda <miaoda@baidu.com>
2026-03-01 12:00:05 +0530

    nutrifuel

--- src/db/supabase.ts
+++ src/db/supabase.ts
@@ -5,4 +5,5 @@
             const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
             export const supabase = createClient(supabaseUrl, supabaseAnonKey);
-            
+            console.log("SUPABASE URL:", import.meta.env.VITE_SUPABASE_URL);
+            console.log("SUPABASE KEY:", import.meta.env.VITE_SUPABASE_ANON_KEY);
